e4d275f85ae53993794ed7f58544e13eccc6a209
mbastard <matteobastard@gmail.com>
2022-06-12 17:26:16 +0200

    exit fixed

--- src/main.c
+++ src/main.c
@@ -3,21 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gborne <gborne@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mbastard <mbastard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 22:54:40 by mbastard          #+#    #+#             */
-/*   Updated: 2022/05/27 19:12:58 by gborne           ###   ########.fr       */
+/*   Updated: 2022/06/12 13:33:13 by mbastard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-
-// write "exit\n" and exit the program
-void	end()
-{
-	write(1, "exit\n", 5);
-	exit(0);
-}
 
 void	clear_cmd(void *content)
 {
@@ -34,7 +27,7 @@ void	clear_data(t_data *data)
 	free(data->cmds);
 }
 
-int	main(int argc, char **argv, char **envp)
+int	main(int argc __unused, char **argv __unused, char **envp __unused)
 {
 	char	*input;
 	t_data	data;
@@ -43,14 +36,10 @@ int	main(int argc, char **argv, char **envp)
 	while (argv && argc)
 	{
 		input = readline(CYELLOW "minishell$ " RESET);
-		if (ft_strlen(input) > 0)
+		// if (ft_strlen(input) > 0)
 			//manage_history(input);
-		if (!ft_strncmp(input, "exit", 4))
-		{
-			clear_data(&data);
-			exit(0);
-		}
-
+		if (!ft_strncmp(input, "exit", 5))
+			quit("exit\n", 0, 0);
 		if (ft_strlen(input) > 0)
 		{
 			get_cmds(input, &data);
@@ -63,40 +52,13 @@ int	main(int argc, char **argv, char **envp)
 	return (0);
 }
 
-/*void manage_history(char *input)
+void	quit(char *error_message, int error_code, int clean)
 {
-	int		history_fd;
-
-	history_fd = open("tmp/history", O_RDWR | O_CREAT | O_APPEND, 0777);
-	add_history(input);
-	ft_putendl_fd(input, history_fd);
-	close(history_fd);
+	if (clean >= 2)
+		;
+	if (error_message)
+		ft_putstr_fd(error_message, 1);
+	if (clean == 1)
+		free(error_message);
+	exit(error_code);
 }
-
-void	recover_history(int fd)
-{
-	int		i;
-	size_t	len;
-	char	*commands;
-	char	**history;
-
-	i = -1;
-	len = 0;
-	commands = get_next_line(fd);
-	if (commands)
-	{
-		while (len != ft_strlen(commands))
-		{
-			len = ft_strlen(commands);
-			commands = ft_strjoin(commands, get_next_line(fd));
-		}
-		write(1, "test\n", 5);
-		history = ft_split(commands, '\n');
-		if (history)
-			while (history[++i])
-				add_history(history[i]);
-		free_tab(history);
-	}
-	free(commands);
-}
-*/
