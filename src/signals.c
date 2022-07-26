c5576a146acb41bbbe1b2e9459193b284c475b41
gregooz42 <gregborne.pro@gmail.com>
2022-07-26 13:02:45 +0200

    fix ctrl+C

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: gborne <gborne@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 02:50:52 by gborne            #+#    #+#             */
-/*   Updated: 2022/07/26 12:53:19 by gborne           ###   ########.fr       */
+/*   Updated: 2022/07/26 13:01:53 by gborne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 static void	control_c()
 {
 	write(1, "\n", 1);
-	write(1, PROMPT, ft_strlen(PROMPT));
-	rl_redisplay();
+	//write(1, PROMPT, ft_strlen(PROMPT));
+	rl_on_new_line();
 	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 void	signal_controller(int signal)
