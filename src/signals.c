ccdeb06d22f4142ac2dce1023c875f772d725eb2
gregooz42 <gregborne.pro@gmail.com>
2022-08-01 18:19:14 +0200

    + libreadline.a, CTRL+C fix

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: gborne <gborne@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 02:50:52 by gborne            #+#    #+#             */
-/*   Updated: 2022/07/30 20:29:21 by gborne           ###   ########.fr       */
+/*   Updated: 2022/08/01 18:18:36 by gborne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,22 @@
 
 static void	control_c(void)
 {
+	pid_t	pid;
+	int		is_kill;
+
+	pid = wait(NULL);
+	is_kill = 0;
+	while (pid > 0)
+	{
+		kill(pid, SIGTERM);
+		is_kill++;
+		pid = wait(NULL);
+	}
 	write(1, "\n", 1);
-	rl_redisplay();
 	rl_replace_line("", 0);
-	rl_redisplay();
-	write(1, CYELLOW "minishell$ " RESET,
-		ft_strlen(CYELLOW "minishell$ " RESET));
+	rl_on_new_line();
+	if (is_kill == 0)
+		rl_redisplay();
 }
 
 void	signal_controller(int signal)
